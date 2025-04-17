5c23a3d33e66cf8b371a900f395a4861cdfec3f5
Anubhav Ranjan <60106478+Mr-AB007@users.noreply.github.com>
2025-04-17 15:17:18 +0530

    Create RangeSumQuery.java

---
+++ array/RangeSumQuery.java
@@ -0,0 +1,28 @@
+class RangeSumQuery {
+    int [] prefixsum; // -2,-2,1,-4,-3,-4
+    public NumArray(int[] nums ) {//-2, 0, 3, -5, 2, -1  //1-4
+        int n = nums.length;
+
+        prefixsum = new int[n];
+        prefixsum[0] = nums[0]; 
+    // Fill in the prefix array such that prefix[i] stores the sum of elements from 0 to i
+        for(int i = 1;i < n ; i++){
+            prefixsum[i] = prefixsum[i-1] + nums[i];
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        // If the range starts from index 0, return prefix[right] directly
+        if(left == 0)
+           return prefixsum[right];
+
+        // Otherwise, subtract the prefix sum up to left-1 to get the range sum
+        return prefixsum[right] - prefixsum[left-1];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
